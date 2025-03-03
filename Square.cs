2bbc2ba7d73dab76ec888e0ef7a390aa8f2571ac
dnbulykin@yandex.ru <dnbulykin@yandex.ru>
2025-03-03 21:26:32 +0300

    переименованы несколько раз проект, решение и namespace. Немного теории про классы, созданы квадрат и круг. square не запускается, circle запускается...

---
+++ Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class Square
+    {
+        public int size;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int Size 
+        { 
+            get 
+            {
+                return Size;
+            }
+
+            set
+            {
+                size = value;
+            } 
+        }
+    }
+}
