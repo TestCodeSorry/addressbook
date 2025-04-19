1ecb3d0dca396b28e2382ba71205db7948790384
dnbulykin@yandex.ru <dnbulykin@yandex.ru>
2025-04-19 09:44:22 +0300

    #7.1 v3

---
+++ addressbook-test-data-generators/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using WebAddressbookTests;
+using System.Diagnostics.Contracts;
+
+namespace addressbook_test_data_generators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int count = Convert.ToInt32(args[1]);
+            StreamWriter writer = new StreamWriter(args[2]);
+            string format = args[3];
+            string dataType = args[0];
+
+            List<GroupData> groups = new List<GroupData>();
+            List<ContactData> contacts = new List<ContactData>();
+
+            if (dataType == "groups")
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                    {
+                        Header = TestBase.GenerateRandomString(100),
+                        Footer = TestBase.GenerateRandomString(100)
+                    });
+                }
+            }
+            else if (dataType == "contacts") 
+            { 
+                for (int i = 0; i < count; i++)
+                {
+                    contacts.Add(new ContactData(TestBase.GenerateRandomString(10), TestBase.GenerateRandomString(10)));
+                }
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format " + dataType);
+            }
+            
+            //Запись в файл выбранного типа
+            if (format == "csv")
+            {
+                WriteGroupsToCsvFile(groups, writer);
+            }
+            else if (format == "xml")
+            {
+                WriteGroupsToXmlFile(groups, writer);
+            }
+            else if (format == "json")
+            {
+                WriteGroupsToJsonFile(groups, writer);
+                WriteContactsToJsonFile(contacts, writer);
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format " + format);
+            }
+            writer.Close();
+        }
+
+        static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach (GroupData group in groups)
+            {
+                writer.WriteLine(String.Format("${0}, ${1}, ${2}",
+                    group.Name, group.Header, group.Footer));
+            }
+        }
+
+        static void WriteGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        } 
+        
+        static void WriteContactsToJsonFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+}
